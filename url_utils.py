57d074c01084d860b29d37bd82f4a90e7b0f70f8
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2019-04-23 04:15:28 -0800

    url_utils.py updated

--- url_utils.py
+++ url_utils.py
@@ -4,3 +4,5 @@ def gen_from_urls(urls: tuple) -> tuple:
     for resp in(requests.get(url) for url in urls):
         yield len(resp.content), resp.status_code, resp.url
 
+urls = ('http://headfirstlabs.com', 'http://oreilly.com', 'http://twitter.com')
+value = gen_from_urls(urls)
